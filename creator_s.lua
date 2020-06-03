41da2135b412e3092f53cca405adce8b44dfbb2b
Podhal <43972851+NieznajomyPodhal@users.noreply.github.com>
2020-06-03 22:39:38 +0200

    Create creator_s.lua

---
+++ creator_s.lua
@@ -0,0 +1,7 @@
+addEvent("createHouse", true)
+addEventHandler("createHouse", resourceRoot, function(size, genre, cost, name, description)
+	
+end)
+
+-- function abc(...)
+--     exports['mm_db']:query()
